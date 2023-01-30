9db7fbc361b18358aad24c0da852ad6e59287ab0
sookho <howawrdsookho@gmail.com>
2023-01-30 10:46:17 -0500

    week 5 update

---
+++ university/src/main/java/com/example/university/exception/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.example.university.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public String handleException(Exception e) {
+        log.error("Default Exception",e);
+        return "something wrong";
+    }
+}
